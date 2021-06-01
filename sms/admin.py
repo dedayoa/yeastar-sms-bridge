3d6890ff627cb6c3c6c2622b34a92614ac6167ce
Adedayo Ayeni <dedayoa@gmail.com>
2021-06-01 07:21:41 -0600

    action to mark message as new

--- sms/admin.py
+++ sms/admin.py
@@ -3,6 +3,12 @@ from django.contrib import admin
 # Register your models here.
 from .models import SMSMessageStateLog, SMSMessage, InvalidSMSMessage, SMSQueue
 
+
+def mark_messages_as_new(modeladmin, request, queryset):
+    for message in queryset:
+        message.status = SMSMessage.Status.NEW
+        message.save()
+
 class SMSMessageStateLogAdmin(admin.ModelAdmin):
     list_display = ('sms_message_id', 'state', 'state_reason', 'timestamp')
     search_fields = ('sms_message_id',)
@@ -10,8 +16,10 @@ class SMSQueueAdmin(admin.ModelAdmin):
     list_display = ['message', 'submit_attempts', 'next_submit_attempt_at', 'created']
 
 class SMSMessageAdmin(admin.ModelAdmin):
-    list_display = ['id', 'bulk_id', 'text', 'recipient', 'pages', 'send_span', 'status']
+    list_display = ['id', 'bulk_id', 'text', 'recipient', 'pages', 'send_span', 'status', 'created_at']
     search_fields = ('id', 'recipient')
+    list_filter = ('status',)
+    actions = [mark_messages_as_new]
     
     def get_queryset(self, request):
         qs = super(SMSMessageAdmin, self).get_queryset(request)
