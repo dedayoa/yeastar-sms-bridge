30a407abea78d108eed8d669084ccb4ff2e31dfd
Adedayo Ayeni <dedayoa@gmail.com>
2021-06-28 00:39:24 -0600

    link should display sms message detail

--- sms/admin.py
+++ sms/admin.py
@@ -4,6 +4,8 @@ from django.contrib import admin
 from .models import SMSMessageStateLog, SMSMessage, InvalidSMSMessage, SMSQueue
 from import_export import resources
 from import_export.admin import ExportActionModelAdmin
+from django.utils.safestring import mark_safe
+from django.urls import reverse
 
 class SMSMessageResource(resources.ModelResource):
     class Meta:
@@ -16,8 +18,13 @@ def mark_messages_as_new(modeladmin, request, queryset):
         message.save()
 
 class SMSMessageStateLogAdmin(admin.ModelAdmin):
-    list_display = ('sms_message_id', 'state', 'state_reason', 'timestamp')
+    list_display = ('sms_id', 'state', 'state_reason', 'timestamp')
     search_fields = ('sms_message_id',)
+
+    def sms_id(self, obj):
+        obj_link = reverse("admin:%s_%s_change"%(SMSMessage._meta.app_label, SMSMessage._meta.model_name), args=(obj.sms_message_id,))
+        return mark_safe(f'<a href="{obj_link}">{obj.sms_message_id}</a>')
+        
 class SMSQueueAdmin(admin.ModelAdmin):
     list_display = ['message', 'submit_attempts', 'next_submit_attempt_at', 'created']
 
